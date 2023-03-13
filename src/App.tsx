1d77a58978d2302f79fc0865e1b2c3da17c9960d
dev.swing <ongsim123@gmail.com>
2023-03-13 15:38:26 +0900

    Fix : Provider 세팅 및 패키지를 찾지 못하여 npm re install

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { isDarkState } from "store/atoms";
+import { GlobalStyle, defaultTheme, darkTheme } from "styles/theme";
+import SignUp from "routes/SignUp";
+
 const App = () => {
-  return <div>app</div>;
+  const isLight = useRecoilValue(isDarkState);
+  return (
+    <>
+      <ThemeProvider theme={isLight ? defaultTheme : darkTheme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { CookiesProvider } from "react-cookie";
+import { RecoilRoot } from "recoil";
+import { StyledEngineProvider } from "@mui/material";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <StyledEngineProvider injectFirst>
+    <CookiesProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </CookiesProvider>
+  </StyledEngineProvider>
+);
